d585f776c125c6d970110a3c5313c1974264fc06
sugarda07 <60727691+sugarda07@users.noreply.github.com>
2020-02-14 07:21:25 +0700

    Update index.php

--- sugard/index.php
+++ sugard/index.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION['id_admin'])) {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Widgets</title>
+  <title>RuangDigital</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
